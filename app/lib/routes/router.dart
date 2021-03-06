5edf320247510e0628613ea110967cdee182d487
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-03-06 08:05:53 -0800

    Fixed pubspec issues and added screen (#20)

--- app/lib/routes/router.dart
+++ app/lib/routes/router.dart
@@ -1,3 +1,4 @@
+import 'package:app/screens/home/home_view.dart';
 import 'package:app/screens/sign_in/sign_in_view.dart';
 import 'package:app/screens/sign_up/sign_up_view.dart';
 import 'package:app/screens/splash/splash_view.dart';
@@ -11,6 +12,7 @@ import 'package:auto_route/auto_route_annotations.dart';
     MaterialRoute(page: WelcomeView, initial: true),
     MaterialRoute(page: SignInView),
     MaterialRoute(page: SignUpView),
+    MaterialRoute(page: HomeView),
   ],
 )
 class $LetsGetPhysicalRouter {}
